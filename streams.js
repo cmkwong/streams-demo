36c5c8bd62225061d4baccde003bbb8df1c78f6f
cmkwong <cheung.mankwong@yahoo.com.hk>
2020-10-02 01:23:45 +0800

    (37)1. Create a read file stream\n 2. but there are still little problem that the speed of read from file is much faster than sending to browser

--- streams.js
+++ streams.js
@@ -4,9 +4,26 @@ const server = require('http').createServer();
 // Observer
 server.on('request', (req,res)=>{
   // Solution 1
-  fs.readFile("../test-file.txt", (err,data)=>{
-    if (err) console.log(err);
-    res.end(data);
+  // fs.readFile("../test-file.txt", (err,data)=>{
+  //   if (err) console.log(err);
+  //   res.end(data);
+  // });
+
+  // Solution 2 (better)
+  // https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15064754?start=291#notes
+  const readable = fs.createReadStream("../testt-file.txt");
+  readable.on("data", chunk => {
+    res.write(chunk);
+  });
+  readable.on("end", ()=>{
+    res.end();
+  });
+  
+  // Observe the error event
+  readable.on("error", err=>{
+    console.log(err);
+    res.statusCode = 500;
+    res.end("File not found.")
   });
 });
 
